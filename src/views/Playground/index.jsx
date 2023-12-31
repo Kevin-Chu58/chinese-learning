12463b5f285a3baa9c915d7bdd5357431be3e61a
kevin.chu <kevin.chu@gordon.edu>
2023-12-31 16:18:16 -0500

    route test

--- src/views/Playground/index.jsx
+++ src/views/Playground/index.jsx
@@ -1,10 +1,13 @@
 import "./style.css";
 import Logo from "../../components/Logo";
+import Footer from "../../components/Footer";
 
 const Playground = () => {
     return (
         <div className="playgroundContainer">
             <Logo />
+
+            <Footer />
         </div>
     )
 }
